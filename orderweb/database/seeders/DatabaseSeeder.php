96a60a928f41af9d85992fdc15e81fff7267e7ff
Héctor <hctaramos1@soy.sena.edu.co>
2024-01-23 10:47:12 -0500

    Seeders y Factories
    Se Crean Los Seeders De Causal, Observation, Role y Type_Activity, Factories de Usar y Technician.

--- orderweb/database/seeders/DatabaseSeeder.php
+++ orderweb/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Technician;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+    $this->call(RoleSeeder::class);
+    $this->call(CausalSeeder::class);
+    $this->call(ObservationSeeder::class);
+    $this->call(TypeActivitySeeder::class);
+    
+    User::factory()->create(['role_id' => 1]); //Se Crea Un Usuario Con Rol Administrador
+    User::factory(3)->create(['role_id' => 2]); //Se Crean Varios Usuarios Con Rol Supervisor
+    Technician::factory()->create(['especiality' => 'Instalación De Redes']);
+    Technician::factory(2)->create(['especiality' => 'Construcción']);
+    Technician::factory(2)->create(['especiality' => 'Lectura De Redes']);
+    Technician::factory(2)->create(); //Técnico Sin Especialidad
+}
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
 }
